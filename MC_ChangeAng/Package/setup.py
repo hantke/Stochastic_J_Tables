539701d8707896abb7cf687440a5c6687c16f5f3
hantke <el.hantke@gmail.com>
2016-05-27 13:44:01 +0100

    Correct a folder name

--- MC_ChangeAng/Package/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('RandomAngle', sources = ['Core.c'])
-
-setup (name = 'Adv_Calc',
-        version = '1.0',
-        description = 'Random Or. a 3d vector in an angle alpha',
-        ext_modules = [module1])
